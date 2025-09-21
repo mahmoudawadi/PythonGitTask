dbcf2c30fac0e5b243ba1b8aa998cced86bb3a75
MahmoudAwadi <mahmoudawadi113@gmail.com>
2025-09-21 20:19:23 +0300

    Add calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a = 10
+b = 5
+sum = a+b
+sum1 = a-b
+sum2 = a*b
+sum3 = a/b
+print('10 + 5 =',sum)
+print('10 - 5 =',sum1)
+print('10 * 5 =',sum2)
+print('10 / 5 =',sum3)
+print('This calculator was created by Mahmoud')
