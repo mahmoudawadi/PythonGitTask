d139001c85fb9a97461bb73e462426e7113650ea
Mahmoudawadi1 <a220138@students.gu.edu.lb>
2025-10-06 20:26:53 +0300

    Add FibonacciIterative

--- algorithms.py
+++ algorithms.py
@@ -26,3 +26,13 @@ def linear_search(numbers, target):
         if x == target:
             return True
     return False
+def fibonacci_iterative(n):
+    """
+    Time complexity: O(n)
+    """
+    if n == 0:
+        return 0
+    a, b = 0, 1
+    for _ in range(1, n):
+        a, b = b, a + b
+    return b
