1b5788eadba2b63ca34bc918cce6013bad68b75f
Mahmoudawadi1 <a220138@students.gu.edu.lb>
2025-10-06 20:13:14 +0300

    Add findMax

--- algorithms.py
+++ algorithms.py
@@ -7,3 +7,14 @@ def factorial(n):
         result *= i
     return result
 
+def find_max(numbers):
+    """
+    Time complexity: O(n)
+    """
+    if not numbers:
+        return None
+    max_val = numbers[0]
+    for x in numbers[1:]:
+        if x > max_val:
+            max_val = x
+    return max_val
